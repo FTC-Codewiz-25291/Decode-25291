ff24df13abe26bd262615c7bdfe31ac0002ea750
Thomas Jin <83102001+Bob1272001@users.noreply.github.com>
2026-02-11 17:57:52 -0500

    commented out the experimental code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoUpClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoUpClose.java
@@ -144,40 +144,40 @@ public class AutoUpClose extends LinearOpMode {
 
         sleep(5000);
 
-        leftFrontDrive.setPower(-0.35);
-        leftBackDrive.setPower(-0.35);
-        rightFrontDrive.setPower(-0.35);
-        rightBackDrive.setPower(-0.35);
-
-        sleep(2000);
-
-        leftFrontDrive.setPower(0.6);
-        leftBackDrive.setPower(-0.6);
-        rightFrontDrive.setPower(-0.6);
-        rightBackDrive.setPower(0.6);
-
-        sleep(375);
-
-        leftFrontDrive.setPower(0.6);
-        leftBackDrive.setPower(0.6);
-        rightFrontDrive.setPower(-0.6);
-        rightBackDrive.setPower(-0.6);
-
-        sleep(320);
-
-        leftFrontDrive.setPower(0.6);
-        leftBackDrive.setPower(0.6);
-        rightFrontDrive.setPower(0.6);
-        rightBackDrive.setPower(0.6);
-
-        sleep(1250);
-
-        leftFrontDrive.setPower(-0.4);
-        leftBackDrive.setPower(-0.4);
-        rightFrontDrive.setPower(-0.4);
-        rightBackDrive.setPower(-0.4);
-
-        sleep(350);
+//        leftFrontDrive.setPower(-0.35);
+//        leftBackDrive.setPower(-0.35);
+//        rightFrontDrive.setPower(-0.35);
+//        rightBackDrive.setPower(-0.35);
+//
+//        sleep(2000);
+//
+//        leftFrontDrive.setPower(0.6);
+//        leftBackDrive.setPower(-0.6);
+//        rightFrontDrive.setPower(-0.6);
+//        rightBackDrive.setPower(0.6);
+//
+//        sleep(375);
+//
+//        leftFrontDrive.setPower(0.6);
+//        leftBackDrive.setPower(0.6);
+//        rightFrontDrive.setPower(-0.6);
+//        rightBackDrive.setPower(-0.6);
+//
+//        sleep(320);
+//
+//        leftFrontDrive.setPower(0.6);
+//        leftBackDrive.setPower(0.6);
+//        rightFrontDrive.setPower(0.6);
+//        rightBackDrive.setPower(0.6);
+//
+//        sleep(1250);
+//
+//        leftFrontDrive.setPower(-0.4);
+//        leftBackDrive.setPower(-0.4);
+//        rightFrontDrive.setPower(-0.4);
+//        rightBackDrive.setPower(-0.4);
+//
+//        sleep(350);
 
     }
 
